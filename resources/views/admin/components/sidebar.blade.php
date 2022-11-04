e6c11e2a9a9a85c63b9f72a93e3d9a96ec6b9766
Farhan <oyisamfarhan@gmail.com>
2022-11-05 05:36:49 +0700

    hapus menu pengaturan akun

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -104,23 +104,10 @@
 	<li class="menu-header small text-uppercase">
 		<span class="menu-header-text">Pages</span>
 	</li>
-	<li class="menu-item {{ 'admin/pengaturan-akun' == request()->path()
-							|| 'admin/ganti-password' == request()->path() ? 'open' : '' }}">
-		<a href="javascript:void(0);" class="menu-link menu-toggle">
+	<li class="menu-item {{ 'admin/ganti-password' == request()->path() ? 'active' : '' }}">
+		<a href="{{ url('admin/ganti-password') }}" class="menu-link">
 			<i class="menu-icon tf-icons bx bx-dock-top"></i>
-			<div data-i18n="Account Settings">Pengaturan Akun</div>
+			<div data-i18n="Account">Ganti Password</div>
 		</a>
-		<ul class="menu-sub">
-			<li class="menu-item {{ 'admin/pengaturan-akun' == request()->path() ? 'active' : '' }}">
-				<a href="{{ url('admin/pengaturan-akun') }}" class="menu-link">
-					<div data-i18n="Account">Akun</div>
-				</a>
-			</li>
-			<li class="menu-item {{ 'admin/ganti-password' == request()->path() ? 'active' : '' }}">
-				<a href="{{ url('admin/ganti-password') }}" class="menu-link">
-					<div data-i18n="Account">Password</div>
-				</a>
-			</li>
-		</ul>
 	</li>
 </ul>
